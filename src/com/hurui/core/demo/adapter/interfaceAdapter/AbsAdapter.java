f289d476e790b616cb0d101225dcb71b2faa816f
hurui007 <hurui_h@qq.com>
2019-08-24 20:05:11 +0800

    接口适配器，选择性的覆盖接口方法

---
+++ src/com/hurui/core/demo/adapter/interfaceAdapter/AbsAdapter.java
@@ -0,0 +1,32 @@
+package com.hurui.core.demo.adapter.interfaceAdapter;
+
+/**
+ * @titler 在AbsAdapter 我们将Interface4 的方法默认实现
+ * 模式实现就是一个空的实现
+ */
+public abstract class AbsAdapter implements Interface4 {
+
+    @Override
+    public void m1(){
+
+    }
+
+    @Override
+    public void m2(){
+
+    }
+
+    @Override
+    public void m3(){
+
+    }
+
+    @Override
+    public void m4(){
+
+    }
+}
+
+
+
+
